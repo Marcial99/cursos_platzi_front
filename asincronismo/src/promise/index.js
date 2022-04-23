2ce0244618ab144b77d586567a34c565bec2c398
PabloMarcial99 <17031116@itcelaya.edu.mx>
2022-04-23 13:17:01 -0500

    Curso de asincromismo completado

--- asincronismo/src/promise/index.js
+++ asincronismo/src/promise/index.js
@@ -1,6 +1,6 @@
 const somethingWillHappen = () => {
   return new Promise((resolve, reject) => {
-    if (false) {
+    if (true) {
       resolve("hey!");
     } else {
       reject("Ops!");
@@ -24,3 +24,15 @@ const somethingWillHappen2 = () => {
     }
   });
 };
+
+somethingWillHappen2()
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+  .then((response) => {
+    console.log("Array of results", response);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
